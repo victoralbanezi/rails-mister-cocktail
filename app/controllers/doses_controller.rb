cffa097ce32e0492cec081d6743af9d690248d21
victor albanezi <victor.albanezi@hotmail.com>
2020-04-27 03:07:59 -0300

    /

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,18 +7,19 @@ class DosesController < ApplicationController
   def create
     @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail_id = @cocktail.id
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
-
+  end
+  
     def destroy
-      @dose = Dose.find(params[:id])
+       @dose = Dose.find(params[:id])
       @dose.destroy
       redirect_to cocktails_path
     end
-  end
 
   private
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_143159) do
+ActiveRecord::Schema.define(version: 2020_04_27_055850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_04_25_143159) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
   end
 
   create_table "doses", force: :cascade do |t|
