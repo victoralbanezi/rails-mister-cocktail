1f3b057e17f6e9df604a2417222b8bb2987c63f3
victor albanezi <victor.albanezi@hotmail.com>
2020-04-27 03:23:05 -0300

    -

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,5 @@ class Cocktail < ApplicationRecord
   validates :name, uniqueness: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  validates :image_url, presence: true
 end
